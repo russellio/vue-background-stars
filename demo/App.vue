789633f3ae31052eca07f1c5a16e72a4451b7218
Jon Russell <info@jonrussell.dev>
2026-08-11 11:22:09 -0400

    refactor: streamline BackgroundStars component usage and update demo controls icon

--- demo/App.vue
+++ demo/App.vue
@@ -124,9 +124,9 @@ const showStars = shallowRef(true);
           Click the
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
             fill="none"
             stroke="currentColor"
             stroke-width="2"
