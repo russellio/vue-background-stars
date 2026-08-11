0f03b296217df2fec0067f0b37c0f4e5f0a9471b
Jon Russell <info@jonrussell.dev>
2026-08-11 11:24:56 -0400

    refactor: simplify BackgroundStars component usage and improve demo controls icon

--- demo/App.vue
+++ demo/App.vue
@@ -126,13 +126,13 @@ const showStars = shallowRef(true);
             xmlns="http://www.w3.org/2000/svg"
             width="18"
             height="18"
-            viewBox="0 0 18 18"
+            viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-sliders-horizontal-icon lucide-sliders-horizontal"
+            class="lucide lucide-sliders-horizontal-icon lucide-sliders-horizontal mx-0.5"
           >
             <path d="M10 5H3" />
             <path d="M12 19H3" />
